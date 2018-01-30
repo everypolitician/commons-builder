8cfcb7c290a38b7956b1f5d879cd4b4e584ae233
Mark Longair <mhl@pobox.com>
2018-01-30 14:39:53 +0000

    Wrap Wikidata label fetching in a caching object

--- boundary_data.rb
+++ boundary_data.rb
@@ -4,26 +4,60 @@ require 'csv'
 
 require_relative 'results'
 
-# This function returns a multilingual name object for a Wikidata item
-def labels(wikidata_item_id)
-  query = <<~SPARQL
-      SELECT ?name_en ?name_fr WHERE {
-        BIND(wd:#{wikidata_item_id} as ?item)
-        OPTIONAL {
-          ?item rdfs:label ?name_en
-          FILTER(LANG(?name_en) = "en")
+class WikidataLabels
+  def item_with_label(wikidata_item_id)
+    return '[no item]' if wikidata_item_id.nil?
+    "#{label_for(wikidata_item_id)} (#{wikidata_item_id})"
+  end
+
+  def label_for(wikidata_item_id)
+    all_labels = labels_for(wikidata_item_id)
+    return nil if all_labels.nil?
+    preferred_language_order = LANGUAGE_MAP.keys
+    languages = all_labels.keys.sort_by do |l|
+      preferred_language_order.index(l)
+    end
+    return all_labels.values_at(*languages).first
+  end
+
+  def labels_for(wikidata_item_id)
+    return labels_cache[wikidata_item_id] if labels_cache.include?(wikidata_item_id)
+    from_wikidata = begin
+                      fetch_labels(wikidata_item_id)
+                    rescue
+                      nil
+                    end
+    labels_cache[wikidata_item_id] = from_wikidata
+    from_wikidata
+  end
+
+  private
+
+  def labels_cache
+    @labels_cache ||= {}
+  end
+
+  # This function returns a multilingual name object for a Wikidata item
+  def fetch_labels(wikidata_item_id)
+    query = <<~SPARQL
+        SELECT ?name_en ?name_fr WHERE {
+          BIND(wd:#{wikidata_item_id} as ?item)
+          OPTIONAL {
+            ?item rdfs:label ?name_en
+            FILTER(LANG(?name_en) = "en")
+          }
+          OPTIONAL {
+            ?item rdfs:label ?name_fr
+            FILTER(LANG(?name_fr) = "fr")
+          }
         }
-        OPTIONAL {
-          ?item rdfs:label ?name_fr
-          FILTER(LANG(?name_fr) = "fr")
-        }
-      }
-SPARQL
-  result = RestClient.get(URL, params: { query: query, format: 'json' })
-  bindings = JSON.parse(result, symbolize_names: true)[:results][:bindings]
-  result = Row.new(bindings[0]).name_object('name', LANGUAGE_MAP)
-  raise "No language labels found for #{wikidata_item_id}" if result.empty?
-  result
+  SPARQL
+    result = RestClient.get(URL, params: { query: query, format: 'json' })
+    bindings = JSON.parse(result, symbolize_names: true)[:results][:bindings]
+    result = Row.new(bindings[0]).name_object('name', LANGUAGE_MAP)
+    raise "No language labels found for #{wikidata_item_id}" if result.empty?
+    result
+  end
 end
 
 # This class parses the metadata we have about boundaries associated
@@ -31,6 +65,10 @@ end
 # repository.
 
 class BoundaryData
+
+  def initialize(wikidata_labels)
+    @wikidata_labels = wikidata_labels
+  end
 
   def name_object(name_columns, feature_data)
     name_columns.map do |locale, column|
@@ -55,6 +93,8 @@ class BoundaryData
     wikidata_to_ms_fb[area_wikidata_item_id]
   end
 
+  attr_reader :wikidata_labels
+
   private
 
   attr_reader :position_item_id
@@ -62,7 +102,7 @@ class BoundaryData
   def popolo_areas_before_parent_mapping
     @popolo_areas_before_parent_mapping ||= index_data.flat_map do |metadata|
       directory = metadata[:directory]
-      area_type_names = labels(metadata[:area_type_wikidata_item_id])
+      area_type_names = wikidata_labels.labels_for(metadata[:area_type_wikidata_item_id])
       name_columns = metadata[:name_columns]
       shapefile_csv = boundaries_dir.join(directory, "#{directory}.csv")
       CSV.read(shapefile_csv, headers: true).map(&:to_h).map do |feature_data|
