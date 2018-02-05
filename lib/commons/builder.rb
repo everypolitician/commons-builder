938ca98742486674fcb73a6189c494638ece49af
Louise Crow <louise.crow@gmail.com>
2018-02-05 18:11:47 +0000

    Load config before other lib files

--- lib/commons/builder.rb
+++ lib/commons/builder.rb
@@ -1,8 +1,8 @@
 require 'commons/builder/version'
+require 'commons/builder/config'
 require 'commons/builder/results'
 require 'commons/builder/labels'
 require 'commons/builder/boundary_data'
-require 'commons/builder/config'
 require 'commons/builder/queries'
 
 module Commons
