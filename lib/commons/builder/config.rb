0931f4b216fe517e9ee7dba2404b25abccd41276
Louise Crow <louise.crow@gmail.com>
2018-02-05 18:10:53 +0000

    Don't use paths relative to lib files

--- lib/commons/builder/config.rb
+++ lib/commons/builder/config.rb
@@ -4,6 +4,6 @@ require 'json'
 require 'pathname'
 
 CONFIG = JSON.parse(
-  Pathname.new(__FILE__).dirname.join('..').join('config.json').read,
+  Pathname.new('config.json').read,
   symbolize_names: true
 )
