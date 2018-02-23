9d749afbf41875354f44863cd5c282c7e549725d
Louise Crow <louise.crow@gmail.com>
2018-02-23 13:45:14 +0000

    Allow config values to be accessed

---
+++ test/commons/config_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class Commons::ConfigTest < Minitest::Test
+
+  def test_can_access_json_values
+    config = Config.new('test/fixtures/config/config.json').values
+    assert_equal('Q23666', config[:country_wikidata_id])
+  end
+
+end
