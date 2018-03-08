c0ff549c6abcaf9a81b1903adceeb32bcc96f6e4
Louise Crow <louise.crow@gmail.com>
2018-03-08 08:32:50 +0000

    Move membership extraction to MembershipData

--- lib/commons/builder/membership_data.rb
+++ lib/commons/builder/membership_data.rb
@@ -74,4 +74,30 @@ class MembershipData
     end.uniq.sort_by { |o| o[:id] }
   end
 
+  def organizations
+    entity_organizations + party_organizations
+  end
+
+  def memberships
+    membership_rows.map do |membership|
+      {
+        id: membership[:statement].value,
+        person_id: membership[:item].value,
+        on_behalf_of_id: membership[:party]&.value,
+        organization_id: membership[:org].value,
+        area_id: membership[:district]&.value,
+        start_date: membership[:start]&.value,
+        end_date: membership[:end]&.value,
+        role_superclass_code: membership[:role_superclass]&.value,
+        role_superclass: membership[:role_superclass] && membership.name_object('role_superclass'),
+        role_code: membership[:role].value,
+        role: membership.name_object('role'),
+      }.reject { |_, v| v.to_s.empty? }
+      # The id here is the statement UUID, so you would have thought
+      # that was enough to predictably order these, However, the
+      # on_behalf_of_id might be from a P102 on the *person* so we
+      # need to add that too.
+    end.uniq.sort_by { |m| [m[:id], m[:on_behalf_of_id]] }
+  end
+
 end
