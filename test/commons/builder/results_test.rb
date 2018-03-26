09fb59149333b0e55c0e07e5254df532ecafe616
Louise Crow <louise.crow@gmail.com>
2018-03-26 19:43:02 +0100

    Add a simple regression test

---
+++ test/commons/builder/results_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class Commons::ResultsTest < Minitest::Test
+
+  def test_row_name_object_extracts_simple_values
+    data = { :party_name_en => { :"xml:lang" => "en",
+                                 :type => "literal",
+                                 :value => "Kuomintang" }
+            }
+    language_map = {
+      "lang:en_US": "en"
+    }
+    row = WikidataRow.new(data, language_map)
+    expected = { :"lang:en_US" => "Kuomintang" }
+    assert_equal(expected, row.name_object('party_name'))
+  end
+
+end
