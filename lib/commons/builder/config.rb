4a5419408a98105b4d607597b9b408361e88c904
Alexander Dutton <alex.dutton@mysociety.org>
2018-04-04 17:58:04 +0100

    Config now exposes config vars through methods
    To support moving away from `language_map` to `languages` keys in
    `config.json`, this adds methods to access `languages` and
    `wikidata_country_id`, with the first inferred (with a warning) from
    `language_map` if `languages` isn't present.
    
    Also added tests for the new Config methods.

--- lib/commons/builder/config.rb
+++ lib/commons/builder/config.rb
@@ -5,7 +5,6 @@ require 'pathname'
 
 
 class Config
-
   attr_reader :values
 
   def initialize(file)
@@ -15,4 +14,18 @@ class Config
     )
   end
 
+  def languages
+    @languages ||= begin
+      if values[:languages]
+        values[:languages]
+      else
+        puts "WARNING: config.json should now use a list of language codes"
+        values[:language_map].values
+      end
+    end
+  end
+
+  def country_wikidata_id
+    @country_wikidata_id ||= values[:country_wikidata_id]
+  end
 end
--- test/commons/builder/config_test.rb
+++ test/commons/builder/config_test.rb
@@ -3,8 +3,22 @@ require 'test_helper'
 class Commons::ConfigTest < Minitest::Test
 
   def test_can_access_json_values
-    config = Config.new('test/fixtures/config/config.json').values
-    assert_equal('Q23666', config[:country_wikidata_id])
+    config = Config.new('test/fixtures/config/config.json')
+    assert_equal('Q23666', config.values[:country_wikidata_id])
   end
 
+  def test_can_access_languages
+    config = Config.new('test/fixtures/config/config.json')
+    assert_equal(['en', 'es'], config.languages)
+  end
+
+  def test_can_access_country_wikidata_id
+    config = Config.new('test/fixtures/config/config.json')
+    assert_equal('Q23666', config.country_wikidata_id)
+  end
+
+  def test_can_infer_languages_from_language_map
+    config = Config.new('test/fixtures/config/config-with-language-map.json')
+    assert_equal(['en', 'es'], config.languages)
+  end
 end
