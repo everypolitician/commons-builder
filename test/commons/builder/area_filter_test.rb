2805d70bec38555734a77cbc152ca418ca6e16f1
Mark Longair <mhl@pobox.com>
2018-05-02 19:09:34 +0100

    Fix RuboCop's Style/ClassAndModuleChildren errors

--- test/commons/builder/area_filter_test.rb
+++ test/commons/builder/area_filter_test.rb
@@ -2,50 +2,52 @@
 
 require 'test_helper'
 
-class Commons::AreaFilterTest < Minitest::Test
-  def test_factory_for_returns_identity_filter_for_nil_param
-    filter = AreaFilterFactory.for(nil)
-    assert_kind_of(AreaIdentityFilter, filter)
-  end
+module Commons
+  class AreaFilterTest < Minitest::Test
+    def test_factory_for_returns_identity_filter_for_nil_param
+      filter = AreaFilterFactory.for(nil)
+      assert_kind_of(AreaIdentityFilter, filter)
+    end
 
-  def test_factory_for_returns_match_filter_for_hash_with_parent_key
-    filter = AreaFilterFactory.for(parent: 'xxx')
-    assert_kind_of(AreaMatchFilter, filter)
-  end
+    def test_factory_for_returns_match_filter_for_hash_with_parent_key
+      filter = AreaFilterFactory.for(parent: 'xxx')
+      assert_kind_of(AreaMatchFilter, filter)
+    end
 
-  def test_factory_for_returns_match_filter_for_hash_with_match_and_column_keys
-    filter = AreaFilterFactory.for(match: 'xxx', column: 'MS_FB')
-    assert_kind_of(AreaMatchFilter, filter)
-  end
+    def test_factory_for_returns_match_filter_for_hash_with_match_and_column_keys
+      filter = AreaFilterFactory.for(match: 'xxx', column: 'MS_FB')
+      assert_kind_of(AreaMatchFilter, filter)
+    end
 
-  def test_factory_for_raises_error_for_hash_with_other_key
-    error = assert_raises { AreaFilterFactory.for(something: 'xxx') }
-    expected_message = 'Unknown filter specification: {:something=>"xxx"}'
-    assert_equal(expected_message, error.message)
-  end
+    def test_factory_for_raises_error_for_hash_with_other_key
+      error = assert_raises { AreaFilterFactory.for(something: 'xxx') }
+      expected_message = 'Unknown filter specification: {:something=>"xxx"}'
+      assert_equal(expected_message, error.message)
+    end
 
-  def test_area_identity_filter_should_include_returns_true
-    assert_equal(true, AreaIdentityFilter.new.should_include?({}))
-  end
+    def test_area_identity_filter_should_include_returns_true
+      assert_equal(true, AreaIdentityFilter.new.should_include?({}))
+    end
 
-  def test_area_match_filter_returns_false_if_regex_not_matched_in_column
-    filter = AreaMatchFilter.new(/xxx/, 'MS_FB')
-    assert_equal(false, filter.should_include?('MS_FB' => 'yyy'))
-  end
+    def test_area_match_filter_returns_false_if_regex_not_matched_in_column
+      filter = AreaMatchFilter.new(/xxx/, 'MS_FB')
+      assert_equal(false, filter.should_include?('MS_FB' => 'yyy'))
+    end
 
-  def test_area_match_filter_returns_false_if_regex_matched_in_other_column
-    filter = AreaMatchFilter.new(/xxx/, 'MS_FB')
-    assert_equal(false, filter.should_include?('MS_FB_PARE' => 'xxx',
-                                               'MS_FB' => 'yyy'))
-  end
+    def test_area_match_filter_returns_false_if_regex_matched_in_other_column
+      filter = AreaMatchFilter.new(/xxx/, 'MS_FB')
+      assert_equal(false, filter.should_include?('MS_FB_PARE' => 'xxx',
+                                                 'MS_FB' => 'yyy'))
+    end
 
-  def test_area_match_filter_returns_true_if_regex_matched_in_column
-    filter = AreaMatchFilter.new(/xxx/, 'MS_FB')
-    assert_equal(true, filter.should_include?('MS_FB' => 'xxx'))
-  end
+    def test_area_match_filter_returns_true_if_regex_matched_in_column
+      filter = AreaMatchFilter.new(/xxx/, 'MS_FB')
+      assert_equal(true, filter.should_include?('MS_FB' => 'xxx'))
+    end
 
-  def test_area_match_filter_returns_true_if_string_matched_in_column
-    filter = AreaMatchFilter.new('xxx', 'MS_FB')
-    assert_equal(true, filter.should_include?('MS_FB' => 'xxx'))
+    def test_area_match_filter_returns_true_if_string_matched_in_column
+      filter = AreaMatchFilter.new('xxx', 'MS_FB')
+      assert_equal(true, filter.should_include?('MS_FB' => 'xxx'))
+    end
   end
 end
--- test/commons/builder/wikidata_test.rb
+++ test/commons/builder/wikidata_test.rb
@@ -2,48 +2,50 @@
 
 require 'test_helper'
 
-class Commons::WikidataTest < Minitest::Test
-  def test_accepts_languages
-    languages = ['en']
-    wikidata = Wikidata.new(languages)
-    assert_equal(languages, wikidata.languages)
-  end
+module Commons
+  class WikidataTest < Minitest::Test
+    def test_accepts_languages
+      languages = ['en']
+      wikidata = Wikidata.new(languages)
+      assert_equal(languages, wikidata.languages)
+    end
 
-  def test_lang_select_returns_space_delimited_names
-    languages = ['en']
-    wikidata = Wikidata.new(languages)
-    expected = '?name_en'
-    assert_equal(expected, wikidata.lang_select)
-  end
+    def test_lang_select_returns_space_delimited_names
+      languages = ['en']
+      wikidata = Wikidata.new(languages)
+      expected = '?name_en'
+      assert_equal(expected, wikidata.lang_select)
+    end
 
-  def test_lang_select_converts_hypens
-    languages = ['zh-tw']
-    wikidata = Wikidata.new(languages)
-    expected = '?name_zh_tw'
-    assert_equal(expected, wikidata.lang_select)
-  end
+    def test_lang_select_converts_hypens
+      languages = ['zh-tw']
+      wikidata = Wikidata.new(languages)
+      expected = '?name_zh_tw'
+      assert_equal(expected, wikidata.lang_select)
+    end
 
-  def test_lang_options_returns_optional_filter
-    languages = ['en']
-    wikidata = Wikidata.new(languages)
-    expected = <<~OPTIONAL_CLAUSE
-      OPTIONAL {
-                ?item rdfs:label ?name_en
-                FILTER(LANG(?name_en) = "en")
-              }
-    OPTIONAL_CLAUSE
-    assert_equal(expected.strip, wikidata.lang_options)
-  end
+    def test_lang_options_returns_optional_filter
+      languages = ['en']
+      wikidata = Wikidata.new(languages)
+      expected = <<~OPTIONAL_CLAUSE
+        OPTIONAL {
+                  ?item rdfs:label ?name_en
+                  FILTER(LANG(?name_en) = "en")
+                }
+      OPTIONAL_CLAUSE
+      assert_equal(expected.strip, wikidata.lang_options)
+    end
 
-  def test_lang_options_converts_hypens
-    languages = ['zh-tw']
-    wikidata = Wikidata.new(languages)
-    expected = <<~OPTIONAL_CLAUSE
-      OPTIONAL {
-                ?item rdfs:label ?name_zh_tw
-                FILTER(LANG(?name_zh_tw) = "zh-tw")
-              }
-    OPTIONAL_CLAUSE
-    assert_equal(expected.strip, wikidata.lang_options)
+    def test_lang_options_converts_hypens
+      languages = ['zh-tw']
+      wikidata = Wikidata.new(languages)
+      expected = <<~OPTIONAL_CLAUSE
+        OPTIONAL {
+                  ?item rdfs:label ?name_zh_tw
+                  FILTER(LANG(?name_zh_tw) = "zh-tw")
+                }
+      OPTIONAL_CLAUSE
+      assert_equal(expected.strip, wikidata.lang_options)
+    end
   end
 end
