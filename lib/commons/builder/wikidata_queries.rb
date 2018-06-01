8eec2e832f71ad328cd648b1980b14558d00f3af
Alexander Dutton <alex.dutton@mysociety.org>
2018-06-01 11:03:35 +0100

    Split each query out into its own template
    This will mean these queries will be more readable than they were in
    heredocs, and we avoid `wikidata_queries.rb` getting excessively long
    (unblocking #50). Including other templates and conditional sections are
    also easier.
    
    After this commit, we can also remove the boilerplate code in
    `WikidataQueries` that calls the templates, so that queries can be
    generated directly from across the codebase.

--- lib/commons/builder/wikidata_queries.rb
+++ lib/commons/builder/wikidata_queries.rb
@@ -1,243 +1,99 @@
 # frozen_string_literal: true
 
+require 'liquid'
+
 class WikidataQueries < Wikidata
-  def date_condition(start_date, end_date)
-    return '' unless start_date
-    end_date ||= '9999-12-31'
-    <<~DATE_CONDITION
-      BIND(COALESCE(?start, "1000-01-01T00:00:00Z"^^xsd:dateTime) AS ?start_or_sentinel)
-      BIND(COALESCE(?end, "9999-12-31T00:00:00Z"^^xsd:dateTime) AS ?end_or_sentinel)
-      FILTER (?end_or_sentinel >= "#{start_date}"^^xsd:dateTime)
-      FILTER (?start_or_sentinel <= "#{end_date}"^^xsd:dateTime)
-  DATE_CONDITION
-  end
-
-  def term_condition(term_item_id)
-    return '' unless term_item_id
-    "?statement pq:P2937 wd:#{term_item_id} ."
-  end
-
-  # Use this to generate a `SERVICE wikibase:label` SPARQL pattern, for use in
-  # situations where you want a single label to help the humans that maintain
-  # a democratic commons repository. Do not use it for labels to be presented
-  # to end-users, as that needs potentially multiple language-tagged labels.
-  # For that, use `lang_select` and `lang_options`.
-  def label_service
-    languages_with_en = (['en'] + languages).uniq
-    "SERVICE wikibase:label { bd:serviceParam wikibase:language \"#{languages_with_en.join(',')}\". }"
-  end
-
   def query_legislative(position_item_id:, house_item_id:, term_item_id: nil, start_date: nil, end_date: nil, **_rest)
     unless !!term_item_id ^ !!(start_date and end_date)
       raise 'You must specify either a term item or a start and end date (and not both)'
     end
-    <<~SPARQL
-      SELECT ?statement
-             ?item #{lang_select}
-             ?party #{lang_select('party_name')}
-             ?district #{lang_select('district_name')}
-             ?role #{lang_select('role')}
-             ?role_superclass #{lang_select('role_superclass')}
-             ?start ?end ?facebook
-             ?org #{lang_select('org')} ?org_jurisdiction ?org_seat_count
-      WHERE {
-        BIND(wd:#{position_item_id} as ?role) .
-        BIND(wd:#{house_item_id} as ?org) .
-        #{lang_options('org', '?org')}
-        OPTIONAL {
-          ?org wdt:P1001 ?org_jurisdiction
-        }
-        OPTIONAL {
-          ?org wdt:P1342 ?org_seat_count
-        }
-        ?item p:P39 ?statement .
-        #{lang_options}
-        ?statement ps:P39 ?role .
-        #{lang_options('role', '?role')}
-        OPTIONAL {
-          ?role wdt:P279 ?role_superclass .
-          ?role_superclass wdt:P279+ wd:Q4175034
-          #{lang_options('role_superclass', '?role_superclass')}
-        }
-        #{term_condition(term_item_id)}
-        OPTIONAL { ?statement pq:P580 ?start }
-        OPTIONAL { ?statement pq:P582 ?end }
-        OPTIONAL {
-          ?statement pq:P768 ?district.
-          #{lang_options('district_name', '?district')}
-        }
-        OPTIONAL {
-          ?statement pq:P4100 ?party.
-          #{lang_options('party_name', '?party')}
-        }
-        OPTIONAL { ?item wdt:P2013 ?facebook }
-        #{date_condition(start_date, end_date)}
-      } ORDER BY ?item ?role #{term_item_id ? '?term ' : ''}?start ?end
-  SPARQL
+    templated_query('legislative',
+                    position_item_id: position_item_id,
+                    house_item_id: house_item_id,
+                    term_item_id: term_item_id,
+                    start_date: start_date,
+                    end_date: end_date)
   end
 
   def query_executive(executive_item_id:, positions:, **_rest)
-    space_separated_role_superclass = positions.map { |p| "wd:#{p.position_item_id}" }.join(' ')
-    <<~SPARQL
-      SELECT ?statement
-             ?item #{lang_select}
-             ?party #{lang_select('party_name')}
-             ?district #{lang_select('district_name')}
-             ?role #{lang_select('role')}
-             ?start ?end ?facebook
-             ?role_superclass #{lang_select('role_superclass')}
-             ?org #{lang_select('org')} ?org_jurisdiction
-      WHERE {
-        VALUES ?role_superclass { #{space_separated_role_superclass} }
-        BIND(wd:#{executive_item_id} AS ?org)
-        #{lang_options('org', '?org')}
-        OPTIONAL {
-          ?org wdt:P1001 ?org_jurisdiction
-        }
-        ?item p:P39 ?statement .
-        #{lang_options}
-        ?statement ps:P39 ?role .
-        #{lang_options('role', '?role')}
-        ?role wdt:P279* ?role_superclass .
-        #{lang_options('role_superclass', '?role_superclass')}
-        ?role wdt:P361 ?org .
-        OPTIONAL {
-          ?role p:P1001/ps:P1001 ?district .
-          #{lang_options('district_name', '?district')}
-        }
-        OPTIONAL { ?statement pq:P580 ?start }
-        OPTIONAL { ?statement pq:P582 ?end }
-        BIND(COALESCE(?end, "9999-12-31T00:00:00Z"^^xsd:dateTime) AS ?end_or_sentinel)
-        FILTER(?end_or_sentinel >= NOW())
-        # Find any current party membership:
-        OPTIONAL {
-          ?item p:P102 ?party_statement .
-          ?party_statement ps:P102 ?party .
-          #{lang_options('party_name', '?party')}
-          OPTIONAL { ?party_statement pq:P582 ?end_party }
-          BIND(COALESCE(?end_party, "9999-12-31T00:00:00Z"^^xsd:dateTime) AS ?party_end_or_sentinel)
-          FILTER(?party_end_or_sentinel >= NOW())
-        }
-        OPTIONAL { ?item wdt:P2013 ?facebook }
-      } ORDER BY ?item ?role ?district ?start ?end
-  SPARQL
+    templated_query('executive',
+                    executive_item_id: executive_item_id,
+                    position_item_ids: positions.map(&:position_item_id))
   end
 
   def select_admin_areas_for_country(country)
-    <<~SPARQL
-      SELECT DISTINCT ?primarySort ?adminArea ?adminAreaType {
-        {
-          VALUES (?adminArea ?primarySort ?adminAreaType) { (#{country} 1 wd:Q6256) }
-        } UNION {
-          # Find FLACSen of this country
-          ?adminArea wdt:P17 #{country} ;
-                wdt:P31/wdt:P279* wd:Q10864048
-          VALUES (?primarySort ?adminAreaType) { (2 wd:Q10864048) }
-        } UNION {
-          # Find cities with populations of over 250k
-          ?adminArea wdt:P17 #{country} ;
-             wdt:P31/wdt:P279* wd:Q515 ;
-             wdt:P1082 ?population .
-          FILTER (?population > 250000)
-          # Make sure the city is not also a FLACS
-          MINUS { ?adminArea wdt:P31/wdt:P279* wd:Q10864048 }
-          VALUES (?primarySort ?adminAreaType) { (3 wd:Q515) }
-        }
-      } ORDER BY ?primarySort
-    SPARQL
+    templated_query('select_admin_areas_for_country',
+                    country: country)
   end
 
   def query_legislative_index(country)
-    country = "wd:#{country}" unless country.start_with?('wd:')
-    <<~SPARQL
-      SELECT DISTINCT ?legislature ?legislatureLabel ?country ?countryLabel ?adminArea ?adminAreaLabel ?adminAreaType ?adminAreaTypeLabel ?legislaturePost ?legislaturePostLabel ?numberOfSeats WHERE {
-        {
-          #{select_admin_areas_for_country(country)}
-        }
-
-        ?adminArea wdt:P194/wdt:P527? ?legislature .
-
-        VALUES ?legislatureType { wd:Q11204 wd:Q10553309 }
-        ?legislature wdt:P31/wdt:P279* ?legislatureType .
-        FILTER (?legislatureType != wd:Q11204 || NOT EXISTS { ?legislature wdt:P527 ?legislaturePart . ?legislaturePart  wdt:P31/wdt:P279* wd:Q10553309 })
-
-        # Attempt to find the position for members of the legislature
-        OPTIONAL {
-          # Both "has part" and "has parts of class" seem to be used; with the latter not in keeping with the draft model
-          ?legislature wdt:P527|wdt:P2670 ?legislaturePost .
-          ?legislaturePost wdt:P31/wdt:P279* wd:Q4164871 .
-          # Make sure positions are either legislators or councillors (and so exclude e.g. mayors)
-          FILTER EXISTS {
-            VALUES ?legislaturePostSuperType { wd:Q4175034 wd:Q708492 }
-            ?legislaturePost wdt:P279+ ?legislaturePostSuperType .
-          }
-        }
-        OPTIONAL {
-          ?legislature wdt:P1342 ?numberOfSeats .
-        }
-
-        # Remove legislatures that have ended
-        FILTER NOT EXISTS { ?legislature wdt:P576 ?legislatureEnd . FILTER (?legislatureEnd < NOW()) }
-        #{label_service}
-      } ORDER BY ?primarySort ?country ?adminAreaType ?legislature ?legislaturePost
-    SPARQL
+    templated_query('legislative_index',
+                    country: country)
   end
 
   def query_legislative_index_terms(*houses)
-    houses = houses.map { |house| "wd:#{house}" }.join(' ')
-    <<~SPARQL
-      SELECT DISTINCT ?house ?houseLabel ?legislature ?legislatureLabel ?term ?termLabel ?termStart ?termEnd WHERE {
-        VALUES ?house { #{houses} }
-        ?house (p:P361/ps:P361)* ?legislature .
-            ?baseTerm p:P31|p:P279 [ ps:P279|ps:P31 wd:Q15238777 ; pq:P642 ?legislature ] .
-            OPTIONAL { ?subTerm wdt:P31 ?baseTerm }
-
-        BIND(COALESCE(?subTerm, ?baseTerm) AS ?term)
-
-        OPTIONAL { ?term (wdt:P580|wdt:P571) ?termStart. }
-        OPTIONAL { ?term (wdt:P582|wdt:P576) ?termEnd. }
-        OPTIONAL { ?term (wdt:P155|wdt:P1365) ?termReplaces }
-        OPTIONAL { ?term (wdt:P156|wdt:P1366) ?termReplacedBy }
-
-        FILTER (!BOUND(?termEnd) || ?termEnd > NOW())
-        FILTER (!BOUND(?termReplacedBy))
-        #{label_service}
-      } ORDER BY ?termStart ?term
-    SPARQL
+    templated_query('legislative_index_terms',
+                    houses: houses)
   end
 
   def query_executive_index(country)
-    country = "wd:#{country}" unless country.start_with?('wd:')
-    <<~SPARQL
-      SELECT DISTINCT ?executive ?executiveLabel ?adminArea ?adminAreaLabel ?adminAreaType ?adminAreaTypeLabel ?position ?positionLabel {
-        {
-          #{select_admin_areas_for_country(country)}
-        }
+    templated_query('executive_index',
+                    country: country)
+  end
 
-        OPTIONAL {
-          {
-            ?position p:P1001 [ wikibase:rank ?appliesToJurisdictionRank ; ps:P1001 ?adminArea ] ;
-              wdt:P31/wdt:P279* wd:Q4164871 .
-            FILTER (?appliesToJurisdictionRank != wikibase:DeprecatedRank)
-            FILTER EXISTS {
-              VALUES ?positionSuperclass { wd:Q2285706 wd:Q30461 }
-              ?position wdt:P279* ?positionSuperclass .
-            }
-          } UNION {
-            ?adminArea wdt:P1313 ?position
-          }
+  class LangTag < Liquid::Tag
+    def variable(prefix, lang_code, query = true)
+      variable = "#{prefix}_#{lang_code.tr('-', '_')}"
+      variable = "?#{variable}" if query
+      variable
+    end
+  end
 
+  class LangSelect < LangTag
+    def initialize(_tag_name, prefix, options)
+      @prefix = Liquid::Variable.new(prefix, options)
+    end
+
+    def render(context)
+      context['languages'].map { |l| variable(@prefix.render(context), l) }.join(' ')
+    end
+  end
+
+  class LangOptions < LangTag
+    def initialize(_tag_name, params, options)
+      params = params.split
+      @prefix = Liquid::Variable.new(params[0], options)
+      @item = Liquid::Variable.new(params[1], options)
+    end
+
+    def render(context)
+      context['languages'].map do |l|
+        <<~CLAUSE
           OPTIONAL {
-            ?position wdt:P361 ?executive .
-            # Exclude executives that are privy councils
-            FILTER NOT EXISTS { ?executive wdt:P31/wdt:P279* wd:Q6528244 }
-            # Exclude executives which aren't direct parents of the position
-            FILTER NOT EXISTS { ?position wdt:P361 ?other . ?other wdt:P361+ ?executive }
+            #{@item.render(context)} rdfs:label #{variable(@prefix.render(context), l)}
+            FILTER(LANG(#{variable(@prefix.render(context), l)}) = \"#{l}\")
           }
-        }
+        CLAUSE
+      end.join("\n")
+    end
+  end
 
-        #{label_service}
-      } ORDER BY ?primarySort ?country ?adminAreaType ?executive ?position
-    SPARQL
+  def templated_query_from_string(name, query, options = {})
+    Liquid::Template.file_system = Liquid::LocalFileSystem.new(Pathname.new(__dir__).join('queries'), '%s.rq.liquid')
+    Liquid::Template.register_tag('lang_select', LangSelect)
+    Liquid::Template.register_tag('lang_options', LangOptions)
+
+    @templated_queries ||= {}
+    @templated_queries[name] ||= Liquid::Template.parse(query, error_mode: :strict)
+
+    options = options.map { |k, v| [k.to_s, v] }.to_h
+    options['languages'] = languages
+    options['self'] = self
+
+    @templated_queries[name].render!(options)
+  end
+
+  def templated_query(name, options = {})
+    templated_query_from_string name, Pathname.new(__dir__).join('queries', name + '.rq.liquid').read, options
   end
 end
