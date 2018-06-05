everypolitician/commons-builder

commit 9fb0986d8030edadffa2cf79b05357574eae452a

Pass config through query generation methods to templates
This will support adding more parameters to the config which can then be
used to inform query generation, such as overriding regional
administrative area Wikidata type IDs, and population thresholds.

This also adds two tests for term query generation, as the calls for
those were changed.

test/commons/builder/wikidata_queries_test.rb
@@ -3,9 +3,14 @@
 require 'test_helper'
 
 class WikidataQueriesTest < Minitest::Test
+  def config(languages: ['en'])
+    data = { 'languages': languages, 'country_wikidata_id': 'Q16' }
+    Config.new(data)
+  end
+
   def test_lang_select_expected_result
     lang_options = WikidataQueries::LangSelect.parse('lang_select', "'name' ", nil, Liquid::ParseContext.new)
-    context_data = { 'languages' => %w[en es] }
+    context_data = { 'config' => config(languages: %w[en es]) }
     context = Liquid::Context.new context_data
     expected = '?name_en ?name_es'
     assert_equal(expected, lang_options.render(context))
@@ -13,7 +18,7 @@ class WikidataQueriesTest < Minitest::Test
 
   def test_lang_options_expected_result
     lang_options = WikidataQueries::LangOptions.parse('lang_options', "'name' '?item' ", nil, Liquid::ParseContext.new)
-    context_data = { 'languages' => %w[en es] }
+    context_data = { 'config' => config(languages: %w[en es]) }
     context = Liquid::Context.new context_data
     expected = <<~CLAUSE
       OPTIONAL {
@@ -31,7 +36,7 @@ class WikidataQueriesTest < Minitest::Test
 
   def test_lang_select_expected_result_in_render
     languages = %w[en zh-tw]
-    wikidata_queries = WikidataQueries.new languages
+    wikidata_queries = WikidataQueries.new config(languages: languages)
     template = "{% lang_select 'party' %}"
     expected = '?party_en ?party_zh_tw'
     assert_equal expected, wikidata_queries.templated_query_from_string('q1', template)
@@ -39,7 +44,7 @@ class WikidataQueriesTest < Minitest::Test
 
   def test_lang_options_expected_result_in_render
     languages = %w[en zh-tw]
-    wikidata_queries = WikidataQueries.new languages
+    wikidata_queries = WikidataQueries.new config(languages: languages)
     template = "{% lang_options 'party_name' '?party' %}"
     expected = <<~CLAUSE
       OPTIONAL {
@@ -57,20 +62,20 @@ class WikidataQueriesTest < Minitest::Test
 
   def test_templated_query_with_sym_options
     languages = %w[en es]
-    wikidata_queries = WikidataQueries.new languages
+    wikidata_queries = WikidataQueries.new config(languages: languages)
     assert_equal 'abc', wikidata_queries.templated_query_from_string('q3', 'a{{ foo }}c', foo: 'b')
   end
 
   def test_templated_query_with_string_options
     languages = %w[en es]
-    wikidata_queries = WikidataQueries.new languages
+    wikidata_queries = WikidataQueries.new config(languages: languages)
     assert_equal 'abc', wikidata_queries.templated_query_from_string('q4', 'a{{ foo }}c', 'foo' => 'b')
   end
 
   def test_templated_query_has_languages_available
     languages = %w[en es]
-    wikidata_queries = WikidataQueries.new languages
-    template = '{% for l in languages %}{{ l }}{% unless forloop.last %}, {% endunless %}{% endfor %}'
+    wikidata_queries = WikidataQueries.new config(languages: languages)
+    template = '{% for l in config.languages %}{{ l }}{% unless forloop.last %}, {% endunless %}{% endfor %}'
     assert_equal 'en, es', wikidata_queries.templated_query_from_string('q5', template)
   end
 end

bin/build
@@ -27,7 +27,7 @@ actions = if ARGV.empty?
             ARGV.clone
           end
 
-wikidata_labels = WikidataLabels.new(config.languages)
+wikidata_labels = WikidataLabels.new(config)
 
 boundaries_dir = Dir.exist?('boundaries/build') ? 'boundaries/build' : 'boundaries'
 boundary_data = BoundaryData.new(wikidata_labels, boundaries_dir: boundaries_dir)
@@ -47,7 +47,7 @@ boundary_position_ids = boundary_data.popolo_areas.map { |a| a[:associated_wikid
       raw_results_pathname = output_dir.join('query-results.json')
 
       if actions.include? 'update'
-        sparql_query = term.query(config.languages)
+        sparql_query = term.query(config)
         output_dir.join('query-used.rq').write(sparql_query)
 
         query_params = {

bin/generate_executive_index
@@ -8,9 +8,7 @@ require 'commons/builder'
 
 config = Config.new_from_file('config.json')
 
-executives = Executive.list(config.country_wikidata_id,
-                            config.languages,
-                            save_queries: true)
+executives = Executive.list(config, save_queries: true)
 
 open('executive/index.json', 'w') do |file|
   file.write(JSON.pretty_generate(executives.map(&:as_json)))

bin/generate_legislative_index
@@ -8,9 +8,7 @@ require 'commons/builder'
 
 config = Config.new_from_file('config.json')
 
-legislatures = Legislature.list(config.country_wikidata_id,
-                                config.languages,
-                                save_queries: true)
+legislatures = Legislature.list(config, save_queries: true)
 
 open('legislative/index.json', 'w') do |file|
   file.write(JSON.pretty_generate(legislatures.map(&:as_json)))

lib/commons/builder/current_executive.rb
@@ -7,10 +7,10 @@ class CurrentExecutive
 
   attr_accessor :executive
 
-  def query(languages)
-    WikidataQueries.new(languages).templated_query('executive',
-                                                   executive_item_id: executive.executive_item_id,
-                                                   positions: executive.positions.map(&:position_item_id))
+  def query(config)
+    WikidataQueries.new(config).templated_query('executive',
+                                                executive_item_id: executive.executive_item_id,
+                                                position_item_ids: executive.positions.map(&:position_item_id))
   end
 
   def output_relative

lib/commons/builder/executive.rb
@@ -23,10 +23,10 @@ class Executive < Branch
     @positions.map { |t| Position.new(branch: self, **t) }
   end
 
-  def self.list(country_id, languages, save_queries: false)
-    wikidata_queries = WikidataQueries.new(languages)
-    wikidata_labels = WikidataLabels.new(languages)
-    sparql_query = wikidata_queries.templated_query('executive_index', country: country_id)
+  def self.list(config, save_queries: false)
+    wikidata_queries = WikidataQueries.new(config)
+    wikidata_labels = WikidataLabels.new(config)
+    sparql_query = wikidata_queries.templated_query('executive_index')
 
     open('executive/index-query-used.rq', 'w').write(sparql_query) if save_queries
     executives = wikidata_queries.perform(sparql_query)

lib/commons/builder/legislative_term.rb
@@ -12,13 +12,13 @@ class LegislativeTerm
 
   attr_accessor :legislature, :term_item_id, :start_date, :end_date, :comment
 
-  def query(languages)
-    WikidataQueries.new(languages).templated_query('legislative',
-                                                   position_item_id: legislature.position_item_id,
-                                                   house_item_id: legislature.house_item_id,
-                                                   term_item_id: term_item_id,
-                                                   start_date: start_date,
-                                                   end_date: end_date)
+  def query(config)
+    WikidataQueries.new(config).templated_query('legislative',
+                                                position_item_id: legislature.position_item_id,
+                                                house_item_id: legislature.house_item_id,
+                                                term_item_id: term_item_id,
+                                                start_date: start_date,
+                                                end_date: end_date)
   end
 
   def output_relative

lib/commons/builder/legislature.rb
@@ -17,9 +17,9 @@ class Legislature < Branch
     @terms.map { |t| LegislativeTerm.new(legislature: self, **t) }
   end
 
-  def self.list(country_id, languages, save_queries: false)
-    wikidata_queries = WikidataQueries.new(languages)
-    sparql_query = wikidata_queries.templated_query('legislative_index', country: country_id)
+  def self.list(config, save_queries: false)
+    wikidata_queries = WikidataQueries.new(config)
+    sparql_query = wikidata_queries.templated_query('legislative_index')
 
     open('legislative/index-query-used.rq', 'w').write(sparql_query) if save_queries
     legislatures = wikidata_queries.perform(sparql_query)

commit 54632659bf4bf92774de5c66d7cf85302a9f6e18

Fix regression: only 'en' was appearing in label service template output
The regression was introduced in 9fb0986 when `languages` was removed
from the context and replaced with `config`, making `languages`
available as `config.languages`, without updating
`lib/commons/builder/queries/label_service.rq.liquid`.

Added a regression test, but we should also ensure that in general
attempting to access variables not in the context should raise an
exception.

lib/commons/builder/queries/label_service.rq.liquid
@@ -6,6 +6,6 @@
   For that, use `lang_select` and `lang_options`.
 {% endcomment -%}
 SERVICE wikibase:label { bd:serviceParam wikibase:language "en
-  {%- for language in languages %}
+  {%- for language in config.languages %}
     {%- if language != "en" %},{{ language }}{% endif %}
   {%- endfor %}". }

test/commons/builder/wikidata_queries_test.rb
@@ -78,4 +78,12 @@ class WikidataQueriesTest < Minitest::Test
     template = '{% for l in config.languages %}{{ l }}{% unless forloop.last %}, {% endunless %}{% endfor %}'
     assert_equal 'en, es', wikidata_queries.templated_query_from_string('q5', template)
   end
+
+  def test_label_service_has_all_langs
+    languages = %w[es en zh-tw]
+    wikidata_queries = WikidataQueries.new config(languages: languages)
+    template = "{% include 'label_service' %}"
+    expected = 'SERVICE wikibase:label { bd:serviceParam wikibase:language "en,es,zh-tw". }'
+    assert_equal expected, wikidata_queries.templated_query_from_string('q5', template)
+  end
 end