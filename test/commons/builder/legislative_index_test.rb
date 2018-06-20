ab8057e002516d8025400fed40f5d420beba35dd
Alexander Dutton <alex.dutton@mysociety.org>
2018-06-20 17:11:14 +0100

    Style fixes for legislative_index_test

--- test/commons/builder/legislative_index_test.rb
+++ test/commons/builder/legislative_index_test.rb
@@ -14,34 +14,39 @@ module Commons
     end
 
     def test_legislative_term_item_as_json
-      term = LegislativeTerm.new({ legislature: nil, term_item_id: 'Q123', comment: 'Test term' })
-      assert_equal term.as_json, {
+      term = LegislativeTerm.new legislature: nil, term_item_id: 'Q123', comment: 'Test term'
+      expected = {
         comment:      'Test term',
         term_item_id: 'Q123',
       }
+      assert_equal expected, term.as_json
     end
 
     def test_legislative_term_dates_as_json
-      term = LegislativeTerm.new({ legislature: nil, start_date: '1970-01-01', end_date: '1970-12-31' })
-      assert_equal term.as_json, {
+      term = LegislativeTerm.new legislature: nil, start_date: '1970-01-01', end_date: '1970-12-31'
+      expected = {
         start_date: '1970-01-01',
         end_date:   '1970-12-31',
       }
+      assert_equal expected, term.as_json
     end
 
     def test_legislature_as_json
       term = { comment: 'Term', term_item_id: 'Q3' }
-      legislature = Legislature.new(terms: [term], house_item_id: 'Q1',
-                                    position_item_id: 'Q2', comment: 'Test legislature')
-      assert_equal legislature.as_json, {
+      legislature = Legislature.new terms: [term], house_item_id: 'Q1',
+                                    position_item_id: 'Q2', comment: 'Test legislature'
+      expected = {
         comment:          'Test legislature',
         house_item_id:    'Q1',
         position_item_id: 'Q2',
-        terms: [{
-          comment:      'Term',
-          term_item_id: 'Q3',
-        },],
+        terms: [
+          {
+            comment:      'Term',
+            term_item_id: 'Q3',
+          },
+        ],
       }
+      assert_equal expected, legislature.as_json
     end
 
     def test_legislature_list
