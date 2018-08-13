6188766072b66793b37d9c3a599c937275cd432f
Alexander Dutton <alex.dutton@mysociety.org>
2018-08-13 11:17:47 +0100

    Executive.list> handle one executive having multiple positions.
    Previously this would attempt to create a new Executive object per
    position and then fail to sort them. This now groups all positions for a
    given executive into the same Executive object.
    
    Closes #85.

--- test/commons/builder/executive_test.rb
+++ test/commons/builder/executive_test.rb
@@ -51,4 +51,17 @@ class ExecutiveTest < Minitest::Test
     executives = Executive.list(config)
     assert_equal [], executives
   end
+
+  def test_list_with_two_positions_for_one_executive
+    # If an executive is listed in the results more than once, with multiple positions, group those positions into a
+    # single executive entry. Also ensure that the entries are sorted by executive and position item IDs.
+    stub_request(:post, 'https://query.wikidata.org/sparql')
+      .to_return(body: open('test/fixtures/executive-index-two-positions.srj', 'r'))
+
+    executives = Executive.list(config)
+    assert_equal 2, executives.length
+    assert_equal 'Q2665914', executives[0].executive_item_id
+    assert_equal %w[Q22979263 Q23729452], executives[0].positions.map(&:position_item_id)
+    assert_equal 'Q32859621', executives[1].executive_item_id
+  end
 end
